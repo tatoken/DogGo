7292f16ff69979ad6859acf361bb3c8bb799c23c
Marco <marcolizza1@gmail.com>
2025-07-18 22:22:08 +0200

    Correzione versione base classe Calendar

--- app/src/main/java/com/example/doggo_ourapp/Calendar.kt
+++ app/src/main/java/com/example/doggo_ourapp/Calendar.kt
@@ -1,8 +1,10 @@
 package com.example.doggo_ourapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-import com.example.doggo_ourapp.R
 
-class CalendarFragment : Fragment(R.layout.calendar_layout) {
+class Calendar : Fragment(R.layout.calendar_layout) {
+
 
 }
